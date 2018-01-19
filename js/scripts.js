1e002216e2aef67d2041718d1c251c59d7740632
Nicko Dela Cruz <nickodc96@gamil.com>
2018-01-19 13:52:45 -0800

    cleaned up code but my third rule won't work will look into it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
   function isZero(num) {
-  if(num.includes("3")=== true) {
+  if(num.includes("0")=== true) {
   return "BEEP BEEP!";
   }else{
     return "ERROR";
   }
- }
-
-
+   if(num.includes("3")=== true) {
+    return "BOOP";  //This code is not working i don't know why will look it up.
+  }
+}
 $(document).ready(function() {
   $("#user-form").submit(function(event) {
     event.preventDefault();
