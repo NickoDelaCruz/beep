cbec58b6b7a97339b70b65fc0af9ce8ae10a0e0b
Nicko Dela Cruz <nickodc96@gamil.com>
2018-01-19 12:24:54 -0800

    finished html skeleton now working on js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,1 @@
-$(document).ready(function(){
-  var translatorArray = [];
-  var numbers = []
-  $("#user-form").submit(function(event){
-    var beepboop = $('input#user-input').val();
-    if (str === [])
-  }
+
