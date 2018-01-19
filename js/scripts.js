98ce07cee44f811b940088b9243fb969a27b33d0
Nicko Dela Cruz <nickodc96@gamil.com>
2018-01-19 12:48:06 -0800

    add some js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
 
+  function isZero(num) {
+  if (num.includes("3") === true) {
+    return "BEEP BEEP!";
+  } else {
+    return "SORRY DAVE, CANT DO THAT";
+  }
+    return "boop";
+}
+
+$(document).ready(function() {
+  $("#user-form").submit(function(event) {
+    event.preventDefault();
+    var userNumber = $("input#user-inputNum").val();
+    var result = "";
+
+
+    result = isZero(userNumber);
+    $("#result").text(result);
+
+
+  });
+});
