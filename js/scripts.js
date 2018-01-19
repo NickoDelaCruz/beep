d999e0e43a9ebc11c6f309fd4ea6d3ece86b638f
Nicko Dela Cruz <nickodc96@gamil.com>
2018-01-19 11:35:20 -0800

    add html skeleton and forms

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  var translatorArray = [];
+  var numbers = []
+  $("#user-form").submit(function(event){
+    var beepboop = $('input#user-input').val();
+    if (str === [])
+  }
