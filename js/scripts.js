c8719edcd180e4276843105847c38c61181a1b39
Nicko Dela Cruz <nickodc96@gamil.com>
2018-01-19 14:04:57 -0800

    add 3 returns in js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
   function isZero(num) {
-  if(num.includes("0")=== true) {
-  return "BEEP BEEP!";
-  }else{
-    return "ERROR";
+  if(num.includes(0)=== true) {
+    return "BEEP BEEP!";
   }
-   if(num.includes("3")=== true) {
-    return "BOOP";  //This code is not working i don't know why will look it up.
+  else if (num.includes(3) === true) {
+    return "BOOP";
+  }
+  else {
+    return "Error";
   }
 }
+
 $(document).ready(function() {
   $("#user-form").submit(function(event) {
     event.preventDefault();
