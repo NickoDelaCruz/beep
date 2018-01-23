96907f9342be15fd28095c04392770f0134dc312
Nicko Dela Cruz <nickodc96@gamil.com>
2018-01-22 18:46:50 -0800

    changed javascript code.includes char 1 and 2.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,24 @@
-  function isZero(num) {
-  if(num.includes(0)=== true) {
-    return "BEEP BEEP!";
-  }
-  else if (num % 3 === 0) {
-    return "BOOP";
+function inputtedNumber(num) {
+if(num.includes(1)=== true) {
+  return "  BEEP BEEP! ";
+}
+else if (num % 3 === 0) {
+  return "  Im Sorry Dave Can't Do That  ";
+}
+else if (num.includes(2)=== true) {
+  return "  Boop  ";
   }
   else {
-    return "Error";
+    return "  ERROR!!!  ";
   }
 }
+
 
 $(document).ready(function() {
   $("#user-form").submit(function(event) {
     event.preventDefault();
     var userNumber = $("input#user-inputNum").val();
-    result = isZero(userNumber);
-    $("#result").text(result);
+    result = inputtedNumber(userNumber);
+    $("#result").append(result);
   });
 });
