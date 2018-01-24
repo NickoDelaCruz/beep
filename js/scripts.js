6cae72ed8897c3c891f6111cedca32db89070572
Nicko Dela Cruz <nickodc96@gamil.com>
2018-01-23 17:53:58 -0800

    add loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,28 @@
+var array = [];
+
+
 function inputtedNumber(num) {
-if (num % 3 == 0) {
-  return "Im Sorry Dave Can't Do That ";
-}
-else if (num.includes(0)=== true) {
-  return "  BEEP ";
-}
-else if (num.includes(1)=== true) {
-  return "  Boop  ";
+  for(var i=0; i <= num; i++) {
+    if (i % 3 == 0)
+      array.push("sorry dave");
+    else if (i.toString().includes("0"))
+      array.push("beep");
+    else if (i.toString().includes('1'))
+      array.push("boop");
+    else
+      array.push(i)
   }
-  else {
-    return "  ERROR!!!  ";
-  }
+
+return array;
 }
+
 
 
 $(document).ready(function() {
   $("#user-form").submit(function(event) {
     event.preventDefault();
     var userNumber = $("input#user-inputNum").val();
-    result = inputtedNumber(userNumber);
-    $("#result").append(result);
+    var result = inputtedNumber(userNumber);
+    $("#result").text(result);
   });
 });
